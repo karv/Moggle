e4c544a6c216b430fe5b7a54a90815c850fddc6b
Edgar Carballo <karvayoEdgar@gmail.com>
2017-08-09 00:56:08 -0700

    Actually invoke thread's update and draw

--- Game.cs
+++ Game.cs
@@ -20,6 +20,24 @@ namespace Moggle
 		}
 
 		/// <summary>
+		/// Invokes active thread's updater.
+		/// </summary>
+		protected override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+		{
+			base.Update(gameTime);
+			Threads.ActiveThread.UpdateRecursively(gameTime);
+		}
+
+		/// <summary>
+		/// Invokes active thread's draw.
+		/// </summary>
+		protected override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+		{
+			base.Draw(gameTime);
+			Threads.ActiveThread.DrawRecursively();
+		}
+
+		/// <summary>
 		/// Dispose the game.
 		/// </summary>
 		protected override void Dispose(bool disposing)
