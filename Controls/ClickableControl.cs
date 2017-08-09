b75ec5e618032e1c8079e8ed6c1768c03ad30421
Edgar Carballo <karvayoEdgar@gmail.com>
2017-08-08 22:15:51 -0700

    General part of button in an intermediate class ClickableControl

---
+++ Controls/ClickableControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended.Input.InputListeners;
+
+namespace Moggle.Controls
+{
+	public abstract class ClickableControl : IGameComponent, IDisposable
+	{
+		bool _isInitialized;
+
+		MouseListener _mouseListener;
+
+		protected ClickableControl(MouseListener mouse)
+		{
+			_mouseListener = mouse ?? throw new ArgumentNullException(nameof(mouse));
+		}
+
+		void IGameComponent.Initialize()
+		{
+			if (_isInitialized) return;
+
+			_isInitialized = true;
+			Initialize();
+		}
+
+		protected virtual void Initialize()
+		{
+			_mouseListener.MouseClicked += Mouse_MouseClicked;
+		}
+
+		void Mouse_MouseClicked(object sender, MouseEventArgs e) => OnClick(e);
+
+		protected abstract void OnClick(MouseEventArgs e);
+
+		void IDisposable.Dispose()
+		{
+			Dispose();
+		}
+
+		protected virtual void Dispose()
+		{
+			_mouseListener.MouseClicked -= Mouse_MouseClicked;
+		}
+	}
+}
