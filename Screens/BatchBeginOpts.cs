510315d30d8024c218898b170d3f81cfdbe7d9e8
Edgar Carballo <karvayoEdgar@gmail.com>
2017-08-21 15:00:18 -0700

    Add a struct containig the args of Batch.Begin ScreenBase invokes this during draw initialization.

---
+++ Screens/BatchBeginOpts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Moggle.Screens
+{
+	public struct BatchBeginOpts
+	{
+		public SpriteSortMode BatchSorting;
+		public BlendState Blend;
+		public SamplerState Sampler;
+		public DepthStencilState DepthStencil;
+		public RasterizerState Rasterizer;
+		public Effect Effect;
+		public Func<Matrix> Transformatrix;
+
+		public void Begin(SpriteBatch batch)
+		{
+			batch.Begin(BatchSorting,
+									Blend,
+									Sampler,
+									DepthStencil,
+									Rasterizer,
+									Effect,
+									Transformatrix());
+		}
+	}
+}
